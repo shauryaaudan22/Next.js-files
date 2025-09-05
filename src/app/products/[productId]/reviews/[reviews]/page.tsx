81f33a990cdcccb10500cd8f900c2399a3c13cf2
ShauryaSudan22 <sudanshaurya22@gmail.com>
2025-09-05 13:56:21 +0530

    done 5

--- src/app/products/[productId]/reviews/[reviews]/page.tsx
+++ src/app/products/[productId]/reviews/[reviews]/page.tsx
@@ -4,7 +4,7 @@ export default async function productReview({
   params: { productId: string; reviewId: string };
 }) {
   const productId = params.productId;
-  const reviewId = params.reviewId;
+  const reviewId =  params.reviewId;
 
-  return <h1>Review {reviewId} for product {productId}</h1>;
+  return (<h1>Review {reviewId} for product {productId}</h1>);
 }
