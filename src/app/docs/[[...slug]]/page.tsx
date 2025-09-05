7c078ebe34afefa36617f2a2a04c9618d7bb539c
ShauryaSudan22 <sudanshaurya22@gmail.com>
2025-09-05 14:12:48 +0530

    done 6

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,10 @@
+export default async function docs({params,}:{params: Promise<{slug: string[]}>;})
+{   const {slug} = await params ;
+    if (slug?.length ===2 ){
+        return (<h1>viewing docs for {slug[0]} and {slug[1]}</h1>);
+        
+    }
+    else if (slug?.length ===1 ){
+        return (<h1> Viewing docs for feature {slug[0]} </h1>);
+    }   return <h1>Docs home page</h1>;
+}
